ed197f3803add9b1a5e9831e77d26800085791df
Sumayyah Idris <sumayyah4kadpoly@gmail.com>
2022-09-11 14:29:06 -0700

    Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Print string to a newline using puts function
+ *
+ * Description: Using puts function to print string in a newline
+ *
+ * Return: 1 if error occurs. 0 otherwise
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
